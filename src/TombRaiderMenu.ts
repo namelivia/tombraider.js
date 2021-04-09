bb7ca2e02217c0a8bf8b98331d509fb7bb266124
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-09 12:17:15 +0200

    Fix model loading error

--- src/TombRaiderMenu.ts
+++ src/TombRaiderMenu.ts
@@ -50,7 +50,7 @@ class TombRaiderMenu {
     params: string,
   ) => {
     let modelId = await this.rendering.addModel(model)
-    if (modelId) {
+    if (modelId !== null) {
       let itemsCount = this.item.add(name, model, action, params, modelId)
       this.world.setItemsNumber(itemsCount)
     }
