f6c192b9cfbc419db5080fc99232002945f6cc0d
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-04-14 20:27:55 +0200

    Version bump

--- build.js
+++ build.js
@@ -2,7 +2,7 @@ const { build } = require('esbuild')
 
 build({
   entryPoints: ['./src/index.ts'],
-  outfile: './dist/tombraider.js-v0.0.1.min.js',
+  outfile: './dist/tombraider.js-v0.0.2.min.js',
   minify: true,
   sourcemap: true,
   target: ['chrome89', 'firefox86', 'safari14', 'edge89'],
